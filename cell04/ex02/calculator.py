492e01e49fc4c166249ee2119d7f2087f41f5774
Pongpichaed Thiansiriwongsakul <67011274@kmitl.ac.th>
2025-06-26 23:15:56 +0700

    23.15

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+first_number = int(input("Give me the first number: "))
+second_number = int(input("Give me the second number: "))
+print("Thank you!")
+print(f"{first_number} + {second_number} = {first_number + second_number}")
+print(f"{first_number} - {second_number} = {first_number - second_number}")
+print(f"{first_number} / {second_number} = {first_number // second_number}")
+print(f"{first_number} * {second_number} = {first_number * second_number}")
