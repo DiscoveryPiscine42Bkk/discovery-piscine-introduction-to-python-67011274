5a869300fb5178ce019618d4e21432e6b5eaf7a6
Pongpichaed Thiansiriwongsakul <67011274@kmitl.ac.th>
2025-06-26 23:05:15 +0700

    04

---
+++ cell03/ex01/multiplication_table.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+number = int(input("Enter a number\n"))
+for i in range(10):
+    print(f"{i} x {number} = {i * number}")
---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+hello = input("What you gotta say? : ")
+while hello != "STOP":
+    hello = input("I got that! Anything else? : ")
